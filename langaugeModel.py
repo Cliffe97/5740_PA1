ba675a72c4458f066e5ca55855e94d4e40587d2f
yuzhangguo <stevenguo0425@gmail.com>
2019-09-24 20:09:34 -0400

    unknown and addk

--- langaugeModel.py
+++ langaugeModel.py
@@ -1,5 +1,6 @@
 import math
 import operator
+import numpy as np
 
 
 class Bigram:
@@ -12,7 +13,7 @@ class Bigram:
         with open(file,'r') as f:
             for line in f:
                 prev = '.'
-                line = line.split(' ')
+                line = line.lower().split(' ')
                 for word in line:
                     if word in self.dict1:
                         self.dict1[word] +=1
@@ -28,17 +29,17 @@ class Bigram:
         # print(self.dict2)
 
     def train_with_first_OOV(self, file):
-        self.dict1['/unk'] = 0;
+        self.dict1['/unk'] = 0
         with open(file, 'r') as f:
             for line in f:
                 prev = '.'
-                line = line.split(' ')
+                line = line.lower().split(' ')
                 for word in line:
                     if word in self.dict1:
                         self.dict1[word] += 1
                     else:
                         self.dict1[word] = 0
-                        self.dict1['/unk'] +=1;
+                        self.dict1['/unk'] +=1
 
                     if prev + word in self.dict2:
                         self.dict2[prev + word] += 1
@@ -46,10 +47,11 @@ class Bigram:
                         self.dict2[prev + word] = 1
                     prev = word
 
+
     def train_with_topM(self, file, M):
         with open(file, 'r') as f:
             for line in f:
-                line = line.split(' ')
+                line = line.lower().split(' ')
                 for word in line:
                     self.count += 1
                     word = word.lower()
@@ -58,7 +60,7 @@ class Bigram:
                     else:
                         self.dict1[word] = 1
 
-        items = sorted(dict.items(), key=operator.itemgetter(1))
+        items = sorted(self.dict1.items(), key=operator.itemgetter(1))
         disposed = items[M:]
         self.dict1['/unk'] = 0
         for key, value in disposed:
@@ -68,7 +70,7 @@ class Bigram:
         with open(file, 'r') as f:
             for line in f:
                 prev = '.'
-                line = line.split(' ')
+                line = line.lower().split(' ')
                 for word in line:
                     if word not in self.dict1:
                         word = '/unk'
@@ -80,7 +82,15 @@ class Bigram:
                     else:
                         self.dict2[prev + word] = 1
                     prev = word
+        print(len(self.dict1))
 
+    def test_corpus(self, file, k):
+        res = []
+        with open(file, 'r') as f:
+            for line in f:
+                line = line.lower().split(' ')
+                res.append(self.test_with_addK(k, line))
+        return res
 
 
     def test(self,review):
@@ -102,6 +112,7 @@ class Bigram:
             if prev not in self.dict1:
                 prev = '/unk'
 
+            print(prev + word)
             if prev + word in self.dict2:
                 # print(prev + word)
                 prob += math.log((self.dict2[prev + word] + k) / (self.dict1[word] + k * len(self.dict1)), 2)
@@ -123,7 +134,7 @@ class Unigram:
     def train(self,file):
         with open(file,'r') as f:
             for line in f:
-                line = line.split(' ')
+                line = line.lower().split(' ')
                 for word in line:
                     self.count+=1
                     word = word.lower()
@@ -138,18 +149,27 @@ class Unigram:
             prob += math.log(self.dictionary[word] / self.count, 2)
         return 2**prob
 
+
+
 if __name__ == '__main__':
-    sentence = "we love the location and proximity to everything . The staff was very friendly and courteous . They were so nice to our 2.5 year old boy . got his backpack full of goodies the moment we arrived . We got free wifi and morning drinks by signing up for select guest program . Ca n't beat that ! the only minor issue is the elevator . we have to take 2 separate elevator trips to get to our room . It got a little annoying when we were going in and out often . Otherwise , it was a great stay !"
-
+    sentence = "I want to issue a travel-warning to folks who might sign up for the weekend deal they offer through travelzoo from time to time : The deal says `` free breakfast '' included in the price . However , what they do n't tell you , is that the breakfast consists of a cup of coffee and a bisquit ( or two ) ! Moreover , you need to ask for these `` tickets '' at the lobby when you check in - they wo n't give them to you automatically ! We stayed there over Christmas '03 , and we , and I noticed several guests who bought the same package , had a rather unpleasant experience ! The hotel is nice though , if you do n't consider their lousy service !"
     file_T = 'train/truthful.txt'
-
     file_D = 'train/deceptive.txt'
     sentence = sentence.lower().split(" ")
     model_T = Bigram()
     model_D = Bigram()
     model_D.train_with_first_OOV(file_D)
     model_T.train_with_first_OOV(file_T)
-    print(model_T.test_with_addK(1, sentence))
-    print(model_D.test_with_addK(1, sentence))
-    # print(model_T.test(sent2))
-    # model_D = Unigram(file_D)
+    print(model_T.test_with_addK(1,sentence))
+    print(model_D.test_with_addK(1,sentence))
+
+    # res1 = np.array(model_T.test_with_addK('validation/truthful.txt', 1))
+    # res2 = np.array(model_D.test_corpus('validation/truthful.txt', 1))
+    # a = res1>=res2
+    # unique_elements, counts_elements = np.unique(a, return_counts=True)
+    # print(res1)
+    # print(res2)
+    #
+    # print(unique_elements)
+    # print(counts_elements)
+
